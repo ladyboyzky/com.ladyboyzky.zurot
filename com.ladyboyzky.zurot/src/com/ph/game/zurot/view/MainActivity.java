8d2d7a2d16e6d90c416ca2ae02e6e56223e935e6
hackmel <hackmel@gmail.com>
2012-10-29 22:26:02 +0800

    Added new Objects

--- com.ladyboyzky.zurot/src/com/ph/game/zurot/view/MainActivity.java
+++ com.ladyboyzky.zurot/src/com/ph/game/zurot/view/MainActivity.java
@@ -8,105 +8,117 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
-
+/**
+ * 
+ * @author hackmel
+ * @version 1.0 Date: 28-10-2012
+ * 
+ *          This is the user interface controller
+ */
 public class MainActivity extends Activity {
 
-	
 	private static final String TAG = MainActivity.class.getSimpleName();
 	FrameLayout game;
-	MainGamePanel mainView;
+	FirstLevelGamePanel mainView;
 	LinearLayout gameWidgets;
 
-    Button left ;
-    Button right ;
-    Button up;
-    Button down;
+	Button left;
+	Button right;
+	Button up;
+	Button down;
+
+	/**
+	 * Creates and initialize the user interface
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		game = new FrameLayout(this);
+		mainView = new FirstLevelGamePanel(this);
+		gameWidgets = new LinearLayout(this);
+
+		left = new Button(this);
+		right = new Button(this);
+		up = new Button(this);
+		down = new Button(this);
+
+		left.setWidth(40);
+		left.setText("L");
+		right.setWidth(40);
+		right.setText("R");
+		up.setWidth(40);
+		up.setText("U");
+		down.setWidth(40);
+		down.setText("D");
+
+		gameWidgets.addView(left);
+		gameWidgets.addView(right);
+		gameWidgets.addView(up);
+		gameWidgets.addView(down);
+
+		left.setOnClickListener(leftOnClickListener);
+		right.setOnClickListener(rightOnClickListener);
+		up.setOnClickListener(upOnClickListener);
+		down.setOnClickListener(downOnClickListener);
+
+		game.addView(mainView);
+		game.addView(gameWidgets);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		setContentView(game);
+		Log.d(TAG, "View added");
+	}
+
 	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	
-    	game = new FrameLayout(this);
-        mainView=new MainGamePanel(this);
-    	gameWidgets = new LinearLayout (this);
+	@Override
+	protected void onDestroy() {
+		Log.d(TAG, "Destroying...");
+		super.onDestroy();
+	}
 
-        left = new Button(this);
-        right = new Button(this);
-        up = new Button(this);
-        down = new Button(this);
-        
-        left.setWidth(40);
-        left.setText("L");
-        right.setWidth(40);
-        right.setText("R");
-        up.setWidth(40);
-        up.setText("U");
-        down.setWidth(40);
-        down.setText("D");
-        
-        gameWidgets.addView(left);  
-        gameWidgets.addView(right); 
-        gameWidgets.addView(up); 
-        gameWidgets.addView(down); 
-        
-        
-        left.setOnClickListener(leftOnClickListener);
-        right.setOnClickListener(rightOnClickListener);
-        up.setOnClickListener(upOnClickListener);
-        down.setOnClickListener(downOnClickListener);
-        
-        
+	@Override
+	protected void onStop() {
+		Log.d(TAG, "Stopping...");
+		super.onStop();
+	}
 
-        game.addView(mainView);
-        game.addView(gameWidgets);
-        
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        
-        setContentView(game);
-        Log.d(TAG, "View added");
-    }
-
-    @Override
-    protected void onDestroy() {
-     Log.d(TAG, "Destroying...");
-     super.onDestroy();
-    }
-
-    @Override
-    protected void onStop() {
-     Log.d(TAG, "Stopping...");
-     super.onStop();
-    }
-    
-    
-    private android.view.View.OnClickListener leftOnClickListener = new View.OnClickListener() {
+	/**
+	 * Handles the click event of the Left button
+	 */
+	private android.view.View.OnClickListener leftOnClickListener = new View.OnClickListener() {
 		public void onClick(View v) {
-			MainGamePanel.DIR=1;
+			BasePanel.DIR = 1;
 		}
 	};
-	
-	
-	
-	 private android.view.View.OnClickListener rightOnClickListener = new View.OnClickListener() {
-			public void onClick(View v) {
-				MainGamePanel.DIR=2;
-			}
-		};
-		
-		
-   private android.view.View.OnClickListener upOnClickListener = new View.OnClickListener() {
-				public void onClick(View v) {
-					MainGamePanel.DIR=3;
-				}
-	};	
-	
-	
-   private android.view.View.OnClickListener downOnClickListener = new View.OnClickListener() {
-			public void onClick(View v) {
-				MainGamePanel.DIR=4;
-			}
-    };	
-    
+
+	/**
+	 * Handles the click event of the Right button
+	 */
+	private android.view.View.OnClickListener rightOnClickListener = new View.OnClickListener() {
+		public void onClick(View v) {
+			BasePanel.DIR = 2;
+		}
+	};
+
+	/**
+	 * Handles the click event of the Up button
+	 */
+	private android.view.View.OnClickListener upOnClickListener = new View.OnClickListener() {
+		public void onClick(View v) {
+			BasePanel.DIR = 3;
+		}
+	};
+
+	 /**
+     *  Handles the click event of the Down button
+     */
+	private android.view.View.OnClickListener downOnClickListener = new View.OnClickListener() {
+		public void onClick(View v) {
+			BasePanel.DIR = 4;
+		}
+	};
+
 }
--- com.ladyboyzky.zurot/src/com/ph/game/zurot/view/MainThread.java
+++ com.ladyboyzky.zurot/src/com/ph/game/zurot/view/MainThread.java
@@ -8,25 +8,33 @@ import android.view.SurfaceHolder;
 /**
  * 
  * @author hackmel
- *
+ * @version 1.0
+ * Date: 28-10-2012
+ * 
+ * This is the game loop
  */
 public class MainThread extends Thread {
 	private static final String TAG = MainThread.class.getSimpleName();
 
 	private SurfaceHolder surfaceHolder;
-	private MainGamePanel gamePanel;
+	private FirstLevelGamePanel gamePanel;
 	private boolean running;
 
 	public void setRunning(boolean running) {
 		this.running = running;
 	}
 
-	public MainThread(SurfaceHolder surfaceHolder, MainGamePanel gamePanel) {
+	public MainThread(SurfaceHolder surfaceHolder, FirstLevelGamePanel gamePanel) {
 		super();
 		this.surfaceHolder = surfaceHolder;
 		this.gamePanel = gamePanel;
 	}
 
+	
+	/**
+	 * This keeps the process in order and continuety.
+	 * This is also responsible for the speed of the game
+	 */
 	@Override
 	public void run() {
 		Canvas canvas;
@@ -36,9 +44,10 @@ public class MainThread extends Thread {
 			try {
 				canvas = this.surfaceHolder.lockCanvas();
 				synchronized (surfaceHolder) {
-					sleep(500);
-					gamePanel.updateZurot(MainGamePanel.DIR);
-					this.gamePanel.onDraw(canvas);
+					sleep(300); // controls the speed
+					gamePanel.updateZurot(BasePanel.DIR); // check and update the coordinates of the zurot object
+					gamePanel.checkForCollision();  // Check for collisions
+					this.gamePanel.onDraw(canvas);  // finally re-draw everything again..
 				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
